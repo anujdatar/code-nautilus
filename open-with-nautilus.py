52be60a61f60c24ba5b14aa6406392f5d7280eb7
Anuj Datar <anuj.datar@gmail.com>
2024-05-15 11:21:42 +0530

    Chore: let black auto-format python file
    Reordered imports (built-ins first) and single-quote to double

--- open-with-nautilus.py
+++ open-with-nautilus.py
@@ -5,15 +5,16 @@
 #
 # This script is released to the public domain.
 
-from gi.repository import Nautilus, GObject
-from subprocess import call
 import os
+from subprocess import call
+
+from gi.repository import GObject, Nautilus
 
 # path to application executable
-APP_EXEC = 'MY_CUSTOM_APP_EXEC'
+APP_EXEC = "MY_CUSTOM_APP_EXEC"
 
 # what name do you want to see in the context menu?
-APP_NAME = 'MY_CUSTOM_APP_NAME'
+APP_NAME = "MY_CUSTOM_APP_NAME"
 MENU_ITEM_NAME = APP_NAME.replace(" ", "")
 
 # always create new window?
@@ -23,8 +24,8 @@ NEWWINDOW = False
 class MY_EXTENSION_CLASS_NAME(GObject.GObject, Nautilus.MenuProvider):
 
     def launch_application(self, menu, files):
-        safepaths = ''
-        args = ''
+        safepaths = ""
+        args = ""
 
         for file in files:
             filepath = file.get_location().get_path()
@@ -33,31 +34,31 @@ class MY_EXTENSION_CLASS_NAME(GObject.GObject, Nautilus.MenuProvider):
             # If one of the files we are trying to open is a folder
             # create a new instance of application
             if os.path.isdir(filepath) and os.path.exists(filepath):
-                args = '--new-window '
+                args = "--new-window "
 
         if NEWWINDOW:
-            args = '--new-window '
+            args = "--new-window "
 
-        call(APP_EXEC + ' ' + args + safepaths + '&', shell=True)
+        call(APP_EXEC + " " + args + safepaths + "&", shell=True)
 
     def get_file_items(self, *args):
         files = args[-1]
         item = Nautilus.MenuItem(
-            name=f'{MENU_ITEM_NAME}Open',
-            label=f'Open in {APP_NAME}',
-            tip=f'Opens the selected files with {APP_NAME}'
+            name=f"{MENU_ITEM_NAME}Open",
+            label=f"Open in {APP_NAME}",
+            tip=f"Opens the selected files with {APP_NAME}",
         )
-        item.connect('activate', self.launch_application, files)
+        item.connect("activate", self.launch_application, files)
 
         return [item]
 
     def get_background_items(self, *args):
         file_ = args[-1]
         item = Nautilus.MenuItem(
-            name=f'{MENU_ITEM_NAME}OpenBackground',
-            label=f'Open in {APP_NAME}',
-            tip=f'Opens the current directory in {APP_NAME}'
+            name=f"{MENU_ITEM_NAME}OpenBackground",
+            label=f"Open in {APP_NAME}",
+            tip=f"Opens the current directory in {APP_NAME}",
         )
-        item.connect('activate', self.launch_application, [file_])
+        item.connect("activate", self.launch_application, [file_])
 
         return [item]
